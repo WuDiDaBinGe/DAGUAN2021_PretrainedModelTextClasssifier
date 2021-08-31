f1f8f390c8bff43339ee97b590937b63ad62415c
WuDiDaBinGe <1151680016@qq.com>
2021-08-31 21:43:10 +0800

    yxb:Bert+CNN

---
+++ classification_by_bert/bert_CNN.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/30 下午10:41
+# @Author  : WuDiDaBinGe
+# @FileName: bert_CNN.py
+# @Software: PyCharm
+import torch
+from torch import nn
+from transformers import BertModel
+import torch.nn.functional as F
+
+
+class BertCNN(nn.Module):
+    def __init__(self, config):
+        super(BertCNN, self).__init__()
+        self.config = config
+        self.bert = BertModel.from_pretrained(self.config.bert_local)
+        for param in self.bert.parameters():
+            param.requires_grad = True
+        self.convs = nn.ModuleList(
+            [nn.Conv2d(1, config.num_filters, (k, config.embedding_dim)) for k in config.filter_sizes]
+        )
+        self.dropout = nn.Dropout(config.dropout)
+        self.fc_cnn = nn.Linear(config.num_filters * len(config.filter_sizes), config.second_num_classes)
+
+    def conv_and_pool(self, x, conv):
+        x = F.relu(conv(x)).squeeze(3)
+        x = F.max_pool1d(x, x.size(2)).squeeze(2)
+        return x
+
+    def forward(self, token_ids, mask):
+        outs = self.bert(token_ids, attention_mask=mask)
+        sequence_out = outs[0].unsqueeze(1)
+        out = torch.cat([self.conv_and_pool(sequence_out, conv) for conv in self.convs], 1)
+        out = self.dropout(out)
+        out = self.fc_cnn(out)
+        return out
--- classification_by_bert/config.py
+++ classification_by_bert/config.py
@@ -7,7 +7,7 @@ class Config(object):
     """配置参数"""
 
     def __init__(self, dataset):
-        self.model_name = "classification_by_bert"
+        self.model_name = "bert_cnn"
         self.train_path = dataset + '/datagrand_2021_train.csv'  # 训练集
         self.dev_path = dataset + '/datagrand_2021_train.csv'  # 验证集
         self.test_path = dataset + '/datagrand_2021_test.csv'  # 测试集
@@ -24,7 +24,7 @@ class Config(object):
         self.require_improvement = 50  # 若超过50epoch效果还没提升，则提前结束训练
         self.second_num_classes = 35  # level 2 label
         self.num_epochs = 1000  # epoch数
-        self.batch_size = 32  # mini-batch大小
+        self.batch_size = 12  # mini-batch大小
         self.pad_size = 350  # 每句话处理成的长度(短填长切)
         self.learning_rate = 1e-5  # 学习率
 
@@ -33,5 +33,8 @@ class Config(object):
         self.label_rel = [0, 1, 2, 2, 1, 3, 2, 4, 6, 1, 1, 2, 5, 6, 2, 6, 7, 2, 8, 6, 6, 6, 5, 9, 5, 2, 10, 8, 6, 6, 5,
                           6, 6, 2, 6, 5]
 
-        self.bert_local = dataset + '/MyBert'  # 预训练模型
+        self.bert_local = dataset + '/pretrained_bert_base_9'  # 预训练模型
         self.embedding_dim = 768
+        # Bert CNN
+        self.num_filters = 256
+        self.filter_sizes = (2, 3, 4)  # 卷积核尺寸
