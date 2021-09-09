b536a4dcfda4f84d0333cf49c373d6b3d5761c31
whdcwsj <maxinaerichared@gmail.com>
2021-09-09 15:25:12 +0800

    对抗学习PGD

---
+++ classification_by_bert/model/PGD.py
@@ -0,0 +1,46 @@
+import torch
+
+# Projected Gradient Descent（PGD）
+# FGM 的思路是梯度上升，本质上来说没有什么问题，但是 FGM 简单粗暴的 "一步到位" 是不是有可能并不能走到约束内的最优点呢？当然是有可能的。
+# PGD方法，简单的说，就是 "小步走，多走几步"，如果走出了扰动半径为epsilon的空间，就重新映射回 "球面" 上，以保证扰动不要过大
+class PGD():
+    def __init__(self, model):
+        self.model = model
+        self.emb_backup = {}
+        self.grad_backup = {}
+
+    def attack(self, epsilon=1., alpha=0.3, emb_name='word_embeddings', is_first_attack=False):
+        # emb_name这个参数要换成你模型中embedding的参数名
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and emb_name in name:
+                if is_first_attack:
+                    self.emb_backup[name] = param.data.clone()
+                norm = torch.norm(param.grad)
+                if norm != 0:
+                    r_at = alpha * param.grad / norm
+                    param.data.add_(r_at)
+                    param.data = self.project(name, param.data, epsilon)
+
+    def restore(self, emb_name='word_embeddings'):
+        # emb_name这个参数要换成你模型中embedding的参数名
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and emb_name in name:
+                assert name in self.emb_backup
+                param.data = self.emb_backup[name]
+        self.emb_backup = {}
+
+    def project(self, param_name, param_data, epsilon):
+        r = param_data - self.emb_backup[param_name]
+        if torch.norm(r) > epsilon:
+            r = epsilon * r / torch.norm(r)
+        return self.emb_backup[param_name] + r
+
+    def backup_grad(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                self.grad_backup[name] = param.grad.clone()
+
+    def restore_grad(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                param.grad = self.grad_backup[name]
